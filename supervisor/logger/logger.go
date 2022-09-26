356c190b57ae163c1f00916952394f06af64d431
Nguyen Marc <nguyen_marc@live.fr>
2022-09-26 18:52:23 +0200

    feat(supervisor): better logger format
    Signed-off-by: Nguyen Marc <nguyen_marc@live.fr>

--- supervisor/logger/logger.go
+++ supervisor/logger/logger.go
@@ -1,13 +1,25 @@
 package logger
 
-import "go.uber.org/zap"
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
 var I *zap.Logger
 
 func init() {
-	var err error
-	I, err = zap.NewProduction()
-	if err != nil {
-		panic(err)
-	}
+	atom := zap.NewAtomicLevel()
+	config := zap.NewProductionEncoderConfig() // or zap.NewDevelopmentConfig() or any other zap.Config
+	config.TimeKey = "timestamp"
+	config.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	config.EncodeCaller = zapcore.FullCallerEncoder
+	I = zap.New(zapcore.NewCore(
+		zapcore.NewConsoleEncoder(config),
+		zapcore.Lock(os.Stdout),
+		atom,
+	))
+	atom.SetLevel(zap.ErrorLevel)
 }
