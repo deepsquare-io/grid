a1af455eeffd98ff1a8c84bd98ca8fb797ea8a70
Nguyen Marc <nguyen_marc@live.fr>
2022-10-12 15:16:13 +0200

    fix(cli): install geth
    Signed-off-by: Nguyen Marc <nguyen_marc@live.fr>

--- cli/scripts/0.go.install-generators.sh
+++ cli/scripts/0.go.install-generators.sh
@@ -6,6 +6,3 @@ SCRIPTPATH=$(dirname "$(realpath "$0")")
 PROJECTPATH="${SCRIPTPATH}/../"
 
 cd "${PROJECTPATH}"
-
-go install \
-  github.com/ethereum/go-ethereum/cmd/geth
--- cli/tools/tools.go
+++
@@ -1,7 +0,0 @@
-//go:build tools
-
-package tools
-
-import (
-	_ "github.com/ethereum/go-ethereum"
-)
