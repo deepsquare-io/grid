129f432fb4f08b135d15fbd5f4a6ed86ede3d9ae
Nguyen Marc <nguyen_marc@live.fr>
2022-10-13 12:08:48 +0200

    fix(cli): add debug print
    Signed-off-by: Nguyen Marc <nguyen_marc@live.fr>

--- cli/cmd/submit/submit.go
+++ cli/cmd/submit/submit.go
@@ -1,16 +1,13 @@
 package submit
 
 import (
-	"bytes"
-	"io"
 	"math/big"
-	"mime/multipart"
-	"net/http"
 	"os"
 
 	"github.com/deepsquare-io/the-grid/cli/gen/go/contracts/metascheduler"
 	"github.com/deepsquare-io/the-grid/cli/logger"
 	"github.com/deepsquare-io/the-grid/cli/pkg/eth"
+	"github.com/deepsquare-io/the-grid/cli/pkg/uploader"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
@@ -134,38 +131,14 @@ var Command = &cobra.Command{
 		}()
 
 		// TODO: implements file hash
-		urlResult, err := uploadFile(file, "job.sh")
+		urlResult, err := uploader.UploadFile(file, "job.sh")
 		if err != nil {
 			logger.I.Fatal("couldn't upload file", zap.String("file", args[0]), zap.Error(err))
 		}
+		logger.I.Debug("submit succeeded", zap.String("hash", urlResult))
 
 		jobDefinition.BatchLocationHash = urlResult
 
 		return container.eth.RequestNewJob(c, jobDefinition, amountLocked)
 	},
-}
-
-func uploadFile(file *os.File, fileName string) (string, error) {
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile("asset", fileName)
-	if err != nil {
-		return "", err
-	}
-	_, err = io.Copy(part, file)
-	if err != nil {
-		return "", err
-	}
-	writer.Close()
-
-	res, err := http.Post("https://transfer.sh/", writer.FormDataContentType(), body)
-	if err != nil {
-		return "", err
-	}
-
-	urlResult, err := io.ReadAll(res.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(urlResult), nil
 }
---
+++ cli/pkg/uploader/uploader.go
@@ -0,0 +1,34 @@
+package uploader
+
+import (
+	"bytes"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+func UploadFile(file *os.File, fileName string) (string, error) {
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("asset", fileName)
+	if err != nil {
+		return "", err
+	}
+	_, err = io.Copy(part, file)
+	if err != nil {
+		return "", err
+	}
+	writer.Close()
+
+	res, err := http.Post("https://transfer.sh/", writer.FormDataContentType(), body)
+	if err != nil {
+		return "", err
+	}
+
+	urlResult, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(urlResult), nil
+}
