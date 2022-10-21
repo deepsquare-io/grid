18ef717ecf29c5c7a8deee24526e4e53d995ae09
Nguyen Marc <nguyen_marc@live.fr>
2022-10-21 17:39:58 +0200

    fix(cli): new fixture
    Signed-off-by: Nguyen Marc <nguyen_marc@live.fr>

--- cli/fixtures/tdp-job.sh
+++ cli/fixtures/tdp-job.sh
@@ -3,12 +3,12 @@
 ##SBATCH --gpus=1
 ##SBATCH --mem=10G
 
-ml stdenv Tdp/1.1.0-beta
+ml stdenv Tdp/1.2.0-beta
 
 export DISPLAY=:99
 
 srun --export=ALL \
-  /home/ldap-users/marc/tdp/DeepSquareURSSample.x86_64 \
+  DeepSquareURSSample.x86_64 \
   -maxplayers 8 \
   -renderfps 60 \
   -webserverurl "wss://tdp.deepsquare.run" \
