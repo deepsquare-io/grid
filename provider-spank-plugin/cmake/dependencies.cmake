80e3146a8c9d7a88dd5d166b1f8efa36399198c5
Nguyen Marc <nguyen_marc@live.fr>
2022-11-02 17:11:34 +0100

    feat(spank/jobcomp): decouple grpc build from versioning

---
+++ provider-spank-plugin/cmake/dependencies.cmake
@@ -0,0 +1,25 @@
+include(FetchContent)
+set(FETCHCONTENT_QUIET OFF)
+
+FetchContent_Declare(
+  slurm
+  GIT_REPOSITORY https://github.com/SchedMD/slurm
+  GIT_TAG slurm-22-05-3-1)
+FetchContent_GetProperties(slurm)
+if(NOT slurm_POPULATED)
+  FetchContent_Populate(slurm)
+  execute_process(COMMAND ${slurm_SOURCE_DIR}/configure
+                  WORKING_DIRECTORY ${slurm_SOURCE_DIR})
+endif()
+
+FetchContent_Declare(
+  grpc
+  GIT_REPOSITORY https://github.com/grpc/grpc
+  GIT_TAG v1.49.1)
+FetchContent_GetProperties(grpc)
+if(NOT grpc_POPULATED)
+  FetchContent_Populate(grpc)
+  add_subdirectory(${grpc_SOURCE_DIR} ${grpc_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
+
+find_package(Threads REQUIRED)
