7025d1dd77480209d90695ec108dd1426eba1199
Nguyen Marc <nguyen_marc@live.fr>
2023-02-06 17:55:40 +0100

    fix(sbatch-service): silent flag fix

--- sbatch-service/renderer/renderer_step_run_test.go
+++ sbatch-service/renderer/renderer_step_run_test.go
@@ -88,8 +88,7 @@ STORAGE_PATH='/deepsquare' DEEPSQUARE_INPUT='/deepsquare/input' DEEPSQUARE_OUTPU
   --gpus-per-task=0 \
   --ntasks=1 \
   --gpu-bind=none \
-  /usr/bin/apptainer exec \
-  --silent \
+  /usr/bin/apptainer --silent exec \
   --nv \
   'docker://registry/image' \
   /bin/sh -c 'hostname'`,
@@ -113,8 +112,7 @@ STORAGE_PATH='/deepsquare' DEEPSQUARE_INPUT='/deepsquare/input' DEEPSQUARE_OUTPU
   --gpus-per-task=0 \
   --ntasks=1 \
   --gpu-bind=none \
-  /usr/bin/apptainer exec \
-  --silent \
+  /usr/bin/apptainer --silent exec \
   --nv \
   "$STORAGE_PATH"'/test/my.sqshfs' \
   /bin/sh -c 'hostname'`,
@@ -137,8 +135,7 @@ STORAGE_PATH='/deepsquare' DEEPSQUARE_INPUT='/deepsquare/input' DEEPSQUARE_OUTPU
   --gpus-per-task=0 \
   --ntasks=1 \
   --gpu-bind=none \
-  /usr/bin/apptainer exec \
-  --silent \
+  /usr/bin/apptainer --silent exec \
   --nv \
   '/opt/software/registry/image' \
   /bin/sh -c 'hostname'`,
