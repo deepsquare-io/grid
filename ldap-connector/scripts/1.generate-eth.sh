50aacfc6b5924d536375abeab885eb41906498d4
Lymnah <charly@mancel.net>
2023-02-07 12:51:14 +0100

    feat(smart-contracts): new smart contracts (#171)
    Co-authored-by: Nguyen Marc <nguyen_marc@live.fr>
    Co-authored-by: DonRavle <domagoj@hhtd.de>
    Co-authored-by: DonRavle <105496660+DonRavle@users.noreply.github.com>

--- ldap-connector/scripts/1.generate-eth.sh
+++ ldap-connector/scripts/1.generate-eth.sh
@@ -8,10 +8,10 @@ CONTRACTSPATH="${SCRIPTPATH}/../../smart-contracts"
 
 cd "${CONTRACTSPATH}"
 
-mkdir -p "${PROJECTPATH}/gen/go/contracts/jobmanager"
-solc ./contracts/JobManager.sol \
+mkdir -p "${PROJECTPATH}/gen/go/contracts/metascheduler"
+solc ./contracts/Metascheduler.sol \
   --base-path . \
   --include-path "node_modules/" \
-  --combined-json abi,bin | abigen --pkg jobmanager \
+  --combined-json abi,bin | abigen --pkg metascheduler \
   --combined-json - \
-  --out "${PROJECTPATH}/gen/go/contracts/jobmanager/jobmanager.go"
+  --out "${PROJECTPATH}/gen/go/contracts/metascheduler/metascheduler.go"
