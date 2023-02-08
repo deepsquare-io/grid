8677f87ebbfd92c5629377d556d62afc4b042ca8
Nguyen Marc <nguyen_marc@live.fr>
2023-02-08 14:53:53 +0100

    fix(sbatch-service): unit test stable diffusion

--- sbatch-service/fixtures/fixtures_test.go
+++ sbatch-service/fixtures/fixtures_test.go
@@ -29,6 +29,10 @@ var (
 	fixtureUpscale string
 	//go:embed upscale.txt
 	expectedUpscale string
+	//go:embed stable-diffusion.yaml
+	fixtureStableDiffusion string
+	//go:embed stable-diffusion.txt
+	expecteStableDiffusion string
 )
 
 func TestRenderTDP(t *testing.T) {
@@ -84,5 +88,19 @@ func TestUpscaleJob(t *testing.T) {
 	require.NoError(t, err)
 	fmt.Println(out)
 	require.Equal(t, expectedUpscale, out)
+	require.NoError(t, renderer.Shellcheck(out))
+}
+
+func TestStableDiffusionJob(t *testing.T) {
+	j := struct {
+		Job model.Job
+	}{}
+	err := yaml.Unmarshal([]byte(fixtureStableDiffusion), &j)
+	require.NoError(t, err)
+
+	out, err := renderer.RenderJob(&j.Job)
+	require.NoError(t, err)
+	fmt.Println(out)
+	require.Equal(t, expecteStableDiffusion, out)
 	require.NoError(t, renderer.Shellcheck(out))
 }
