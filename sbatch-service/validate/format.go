71611da02dddf980614bd052e5adc75de58b2d8e
Nguyen Marc <nguyen_marc@live.fr>
2023-03-09 13:02:18 +0100

    fix(sbatch): fix error format print

--- sbatch-service/validate/format.go
+++ sbatch-service/validate/format.go
@@ -9,29 +9,65 @@ import (
 func Format(err validator.FieldError) string {
 	switch err.Tag() {
 	case "url":
-		return fmt.Sprintf("'%s' is not a valid URL (error: '%s')", err.Field(), err.Tag())
+		return fmt.Sprintf(
+			"Field '%s' (='%+v') is not a valid URL {\"error\": \"%s\", \"field\": \"%s\", \"value\": \"%+v\"}",
+			err.Field(), err.Value(), err.Tag(), err.Field(), err.Value(),
+		)
 	case "ip":
-		return fmt.Sprintf("'%s' is not a valid IP (error: '%s')", err.Field(), err.Tag())
+		return fmt.Sprintf(
+			"Field '%s' (='%+v') is not a valid IP {\"error\": \"%s\", \"field\": \"%s\", \"value\": \"%+v\"}",
+			err.Field(), err.Value(), err.Tag(), err.Field(), err.Value(),
+		)
 	case "cidr":
-		return fmt.Sprintf("'%s' is not a valid CIDR (error: '%s')", err.Field(), err.Tag())
+		return fmt.Sprintf(
+			"Field '%s' (='%+v') is not a valid CIDR {\"error\": \"%s\", \"field\": \"%s\", \"value\": \"%+v\"}",
+			err.Field(), err.Value(), err.Tag(), err.Field(), err.Value(),
+		)
 	case "startswith":
-		return fmt.Sprintf("'%s' must start with '%s' (error: '%s')", err.Field(), err.Value(), err.Tag())
+		return fmt.Sprintf(
+			"Field '%s' (='%+v') must start with '%s' {\"error\": \"%s\", \"field\": \"%s\", \"value\": \"%+v\"}",
+			err.Field(), err.Value(), err.Param(), err.Tag(), err.Field(), err.Value(),
+		)
 	case "endsnotwith":
-		return fmt.Sprintf("'%s' must not end with '%s' (error: '%s')", err.Field(), err.Value(), err.Tag())
+		return fmt.Sprintf(
+			"Field '%s' (='%+v') must not end with '%s' {\"error\": \"%s\", \"field\": \"%s\", \"value\": \"%+v\"}",
+			err.Field(), err.Value(), err.Param(), err.Tag(), err.Field(), err.Value(),
+		)
 	case "required":
-		return fmt.Sprintf("'%s' is required (error: '%s')", err.Field(), err.Tag())
+		return fmt.Sprintf(
+			"Field '%s' (='%+v') is required {\"error\": \"%s\", \"field\": \"%s\", \"value\": \"%+v\"}",
+			err.Field(), err.Value(), err.Tag(), err.Field(), err.Value(),
+		)
 	case "valid_envvar_name":
-		return fmt.Sprintf("'%s' is not a valid environment variable (error: '%s')", err.Field(), err.Tag())
+		return fmt.Sprintf(
+			"Field '%s' (='%+v') is not a valid environment variable {\"error\": \"%s\", \"field\": \"%s\", \"value\": \"%+v\"}",
+			err.Field(), err.Value(), err.Tag(), err.Field(), err.Value(),
+		)
 	case "lt":
-		return fmt.Sprintf("'%s' must be less than '%s' (error: '%s')", err.Field(), err.Value(), err.Tag())
+		return fmt.Sprintf(
+			"Field '%s' (='%+v') must be less than '%s' {\"error\": \"%s\", \"field\": \"%s\", \"value\": \"%+v\"}",
+			err.Field(), err.Value(), err.Param(), err.Tag(), err.Field(), err.Value(),
+		)
 	case "gte":
-		return fmt.Sprintf("'%s' must be greater than or equal to '%s' (error: '%s')", err.Field(), err.Value(), err.Tag())
+		return fmt.Sprintf(
+			"Field '%s' (='%+v') must be greater than or equal to '%s' {\"error\": \"%s\", \"field\": \"%s\", \"value\": \"%+v\"}",
+			err.Field(), err.Value(), err.Param(), err.Tag(), err.Field(), err.Value(),
+		)
 	case "valid_container_image_url":
-		return fmt.Sprintf("'%s' is not a valid container image url (error: '%s')", err.Field(), err.Tag())
+		return fmt.Sprintf(
+			"Field '%s' (='%+v') is not a valid container image url {\"error\": \"%s\", \"field\": \"%s\", \"value\": \"%+v\"}",
+			err.Field(), err.Value(), err.Tag(), err.Field(), err.Value(),
+		)
 	case "oneof":
-		return fmt.Sprintf("'%s' must be one of '%s' (error: '%s')", err.Field(), err.Value(), err.Tag())
+		return fmt.Sprintf(
+			"Field '%s' (='%+v') must be one of '%s' {\"error\": \"%s\", \"field\": \"%s\", \"value\": \"%+v\"}",
+			err.Field(), err.Value(), err.Param(), err.Tag(), err.Field(), err.Value(),
+		)
 	case "hostname":
-		return fmt.Sprintf("'%s' is not a valid hostname (error: '%s')", err.Field(), err.Tag())
+		return fmt.Sprintf(
+			"Field '%s' (='%+v') is not a valid hostname {\"error\": \"%s\", \"field\": \"%s\", \"value\": \"%+v\"}",
+			err.Field(), err.Value(), err.Tag(), err.Field(), err.Value(),
+		)
 	}
 	return err.Error()
 }
