8b378e26dbb7ff5747891bf86ebb93169315cc29
Nguyen Marc <nguyen_marc@live.fr>
2023-03-20 02:12:32 +0100

    chore: bump grpc versions

--- provider-spank-plugin/cmake/dependencies.cmake
+++ provider-spank-plugin/cmake/dependencies.cmake
@@ -15,7 +15,7 @@ endif()
 FetchContent_Declare(
   grpc
   GIT_REPOSITORY https://github.com/grpc/grpc
-  GIT_TAG v1.51.0)
+  GIT_TAG v1.52.1)
 FetchContent_GetProperties(grpc)
 if(NOT grpc_POPULATED)
   FetchContent_Populate(grpc)
