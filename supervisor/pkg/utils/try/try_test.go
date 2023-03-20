1d984774aa22e0badcd35324a31bf19b1e23e120
Nguyen Marc <nguyen_marc@live.fr>
2023-03-20 19:31:23 +0100

    fix(supervisor): fix race condition on test

--- supervisor/pkg/utils/try/try_test.go
+++ supervisor/pkg/utils/try/try_test.go
@@ -13,8 +13,8 @@ import (
 
 func TestDoWithContextTimeout(t *testing.T) {
 	err := try.DoWithContextTimeout(context.Background(), 5, time.Millisecond, 1*time.Second, func(ctx context.Context, try int) error {
-		time.Sleep(2 * time.Second)
-		return nil
+		<-ctx.Done()
+		return ctx.Err()
 	})
 
 	require.Equal(t, context.DeadlineExceeded, err)
