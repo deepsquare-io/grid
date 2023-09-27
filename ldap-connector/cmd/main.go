214530030f319ea6a346e6669eadf96b7882a2d5
Nguyen Marc <nguyen_marc@live.fr>
2023-09-27 14:01:06 +0200

    feat(ldap-connector): add GPL3

--- ldap-connector/cmd/main.go
+++ ldap-connector/cmd/main.go
@@ -1,3 +1,18 @@
+// Copyright (C) 2023 DeepSquare Asociation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -94,9 +109,16 @@ var flags = []cli.Flag{
 }
 
 var app = &cli.App{
-	Name:  "ldap-connector",
-	Usage: "Create user on job submit.",
-	Flags: flags,
+	Name:                 "ldap-connector",
+	Usage:                "Create user on job submit.",
+	Flags:                flags,
+	Suggest:              true,
+	EnableBashCompletion: true,
+	Copyright: `ldap-connector  Copyright (C) 2023 DeepSquare Association
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions.
+See the GNU General Public License for more details.`,
 	Action: func(cCtx *cli.Context) error {
 		ctx := cCtx.Context
 		conf, err := config.ParseFile(configPath)
--- ldap-connector/go.mod
+++ ldap-connector/go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/stretchr/testify v1.8.4
 	github.com/urfave/cli/v2 v2.25.7
 	go.uber.org/zap v1.26.0
+	golang.org/x/tools v0.13.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -64,6 +65,7 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.13.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/net v0.15.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
