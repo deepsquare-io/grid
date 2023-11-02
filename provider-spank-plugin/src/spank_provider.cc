160c71bca2b360a065571eaf94beca9a511fc21a
Nguyen Marc <nguyen_marc@live.fr>
2023-11-02 16:50:32 +0100

    fix(spank): fix plugins spank

--- provider-spank-plugin/src/spank_provider.cc
+++ provider-spank-plugin/src/spank_provider.cc
@@ -9,42 +9,15 @@ extern "C" {
 #include "job_api.h"
 #include "report.h"
 #include "slurm_utils.h"
-#include "supervisor/v1alpha1/job.grpc.pb.h"
-
-using supervisor::v1alpha1::SetJobStatusRequest;
 
 extern "C" {
 
-/*
- * These variables are required by the generic plugin interface.  If they
- * are not found in the plugin, the plugin loader will ignore it.
- *
- * plugin_name - a string giving a human-readable description of the
- * plugin.  There is no maximum length, but the symbol must refer to
- * a valid string.
- *
- * plugin_type - a string suggesting the type of the plugin or its
- * applicability to a particular form of data or method of data handling.
- * If the low-level plugin API is used, the contents of this string are
- * unimportant and may be anything.  Slurm uses the higher-level plugin
- * interface which requires this string to be of the form
- *
- *	<application>/<method>
- *
- * where <application> is a description of the intended application of
- * the plugin (e.g., "jobcomp" for Slurm job completion logging) and <method>
- * is a description of how this plugin satisfies that application.  Slurm will
- * only load job completion logging plugins if the plugin_type string has a
- * prefix of "jobcomp/".
- *
- * plugin_version - an unsigned 32-bit integer containing the Slurm version
- * (major.minor.micro combined into a single number).
- */
-extern const char plugin_name[] = "Job completion plugin for providers";
-extern const char plugin_type[] = "jobcomp/provider";
-extern const uint32_t plugin_version = SLURM_VERSION_NUMBER;
+extern const char plugin_name[] = "provider";
+extern const char plugin_type[] = "spank";
+extern const unsigned int plugin_version = SLURM_VERSION_NUMBER;
+extern const unsigned int spank_plugin_version = SLURM_VERSION_NUMBER;
 
-/**
+/*
  * @brief Called at the same time as the job prolog. If this function returns
  * a non-zero value and the SPANK plugin that contains it is required in the
  * plugstack.conf, the node that this is run on will be drained.
@@ -55,7 +28,7 @@ extern const uint32_t plugin_version = SLURM_VERSION_NUMBER;
  * @param argv Argument vector
  * @return int Error code
  */
-extern int slurm_spank_job_prolog(spank_t spank, int ac, char *argv[]) {
+int slurm_spank_job_prolog(spank_t spank, int ac, char *argv[]) {
   // Fetch the job
   unsigned int jobid = 0;
   job_info_msg_t *job_info = NULL;
