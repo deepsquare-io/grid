01dd5246ff742478fd65a36530c402e4a57df65e
Nguyen Marc <nguyen_marc@live.fr>
2024-01-21 15:36:13 +0100

    fix(sbatch): put to integration test

--- sbatch-service/validate/container_image_fetcher_test.go
+++ sbatch-service/validate/container_image_fetcher_test.go
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+//go:build integration
+
 package validate_test
 
 import (
